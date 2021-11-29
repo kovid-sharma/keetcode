9e7e965007aa9a9f8a3f5947e791a983958d822e
kovid-sharma <76726175+kovid-sharma@users.noreply.github.com>
2021-11-29 13:57:14 +0530

    Update minimum-number-of-operations-to-move-all-balls-to-each-box.cpp

--- minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        vector<int> ar;
         int n=boxes.size();
-        for(int i=0;i<n;i++)
-            ar.push_back(boxes[i]-48);
         vector<int> ans;
         for(int i=0;i<n;i++)
         {
             int sum=0;
-            for(int j=i+1;j<n;j++)
+            for(int j=0;j<n;j++)
             {
-                if(ar[j]==1)
+                if(boxes[j]=='1')
                 {
-                    sum+=j-i;
-                }
-            }
-            for(int j=i-1;j>=0;j--)
-            {
-                if(ar[j]==1)
-                {
-                    sum-=j-i;
+                    sum+=abs(j-i);
                 }
             }
             ans.push_back(sum);

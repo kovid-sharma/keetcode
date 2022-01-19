928a33d20493e127c1f08a947e46ae51baad6110
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-01-19 19:27:09 +0530

    Time: 21 ms (9.58%), Space: 7.6 MB (73.74%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -9,17 +9,24 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-       unordered_map<ListNode*,int>mp;
-        while(head!=NULL)
+       //method 2//first was to use map to store that we have reached that listnode once or not this method is standard method using slow and fast pointers what we do is that at some point if there is a cycle the fast and slow eill become same if there is no cycle there will be no cycle and we will return null but after they are matched and there is a cycle we will be resetting our slow pointer to the head
+        if(head==NULL or head->next==NULL)return NULL;
+        
+        ListNode*slower=head;
+        ListNode*fasteer=head;
+        while(fasteer!=NULL and fasteer->next!=NULL)
         {
-            if(mp.find(head)!=mp.end())
-                return head;
-            else
-            {
-                mp[head]=-1;
-                head=head->next;
-            }
-            
+            slower=slower->next;
+            fasteer=fasteer->next->next;
+            if(fasteer==slower)
+                break;
+        }
+        slower=head;
+        while(fasteer!=NULL and fasteer->next!=NULL)
+        {   
+            if(slower==fasteer)return slower;
+            slower=slower->next;
+            fasteer=fasteer->next;
         }
         return NULL;
     }

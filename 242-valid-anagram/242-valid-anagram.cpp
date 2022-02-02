b97c686d2e48982e1968c63ab81f3b5efae099b5
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-02-02 09:57:52 +0530

    Time: 9 ms (66.56%), Space: 7.1 MB (94.75%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t)
-            return true;
-        return false;
+        if(s.size()!=t.size())return false;
+        vector<int>hash(26,0);
+        for(int i=0;i<s.size();i++)
+        {
+            hash[s[i]-'a']--;
+            hash[t[i]-'a']++;
+        }
+        for(int i=0;i<26;i++)
+            if(hash[i]!=0) return false;
+        return true;
     }
 };

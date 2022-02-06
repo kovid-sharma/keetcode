86f5dc1ff33d6066d3c39f741bad4ec021feb2a7
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-02-06 18:46:35 +0530

    Time: 12 ms (23.05%), Space: 10.9 MB (37.19%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
@@ -3,18 +3,16 @@ public:
     int removeDuplicates(vector<int>& nums) {
         //brute
         if(nums.size()<=2)return nums.size();
-        vector<int>cpy;
-        cpy.push_back(nums[0]);
-        cpy.push_back(nums[1]);
+        int indx=2;
         for(int i=2;i<nums.size();i++)
         {
-            if(nums[i]!=nums[i-2])
-            {
-                cpy.push_back(nums[i]);
+            if(nums[i]!=nums[indx-2])
+            {   
+               nums[indx]=nums[i]; 
+               indx++;
             }
         }
-        for(int i=0;i<cpy.size();i++)
-            nums[i]=cpy[i];
-        return cpy.size();
+        
+        return indx;
     }
 };

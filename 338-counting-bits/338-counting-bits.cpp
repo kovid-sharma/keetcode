c438f44d33994bb1dc75cf3f60580d07513e5ed2
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-03-01 13:19:08 +0530

    Time: 11 ms (32.10%), Space: 7.8 MB (89.06%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> arr(n+1);
-        for(int i=0;i<=n;i++)
+        
+        if(n==0) return {0};
+        vector<int>dp(n+1);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
         {
-            arr[i]=__builtin_popcount(i);
+            if(i%2==0)
+                dp[i]=dp[i/2];
+            else
+                dp[i]=1+dp[i/2];
         }
-        return arr;
+        return dp;
     }
 };

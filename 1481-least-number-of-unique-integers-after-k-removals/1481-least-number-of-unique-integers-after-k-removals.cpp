6f722d2c240852c1b036a89b479f7d07c7a229b2
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-03-05 23:17:05 +0530

    Time: 156 ms (94.30%), Space: 62.6 MB (78.68%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
@@ -8,18 +8,12 @@ public:
         for(auto it:mp)
             numfreq.push_back(it.second);
         sort(numfreq.begin(),numfreq.end());
-        int total=arr.size();
         int idx=0;
         while(k>0)
         {   
             if(k<numfreq[idx])
-            {
-                total-=k;
-                k-=k;
-                break;
-            }
+                return numfreq.size()-idx;
             k-=numfreq[idx];
-            total-=numfreq[idx];
             idx++;
         }
         return numfreq.size()-idx;

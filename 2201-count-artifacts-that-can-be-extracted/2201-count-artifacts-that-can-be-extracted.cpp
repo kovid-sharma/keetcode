78d2a27ee9bfb2353800e4552dff04ca1c326728
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-03-13 10:51:11 +0530

    Time: 731 ms (30.44%), Space: 166.1 MB (8.70%) - LeetHub

--- 2201-count-artifacts-that-can-be-extracted/2201-count-artifacts-that-can-be-extracted.cpp
+++ 2201-count-artifacts-that-can-be-extracted/2201-count-artifacts-that-can-be-extracted.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
-    int digArtifacts(int n, vector<vector<int>>& A, vector<vector<int>>& dig) {
-        vector<vector<bool>> M(n, vector<bool>(n)); 
-        for (auto &d : dig) M[d[0]][d[1]] = true;
-        int ans = 0;
-        for (auto &v : A) {
-            bool good = true;
-            for (int x = v[0]; x <= v[2] && good; ++x) {
-                for (int y = v[1]; y <= v[3] && good; ++y) {
-                    if (!M[x][y]) good = false;
+    int digArtifacts(int n, vector<vector<int>>& artifacts, vector<vector<int>>& dig) {
+        int cnt=0;
+        vector<vector<int>>grid(n,vector<int>(n,0));
+        for(int i=0;i<dig.size();i++)
+        {
+            grid[dig[i][0]][dig[i][1]]=-1;
+        }
+        
+        for(int i=0;i<artifacts.size();i++)
+        {   
+            bool ok=true;
+            for(int j=artifacts[i][0];j<=artifacts[i][2];j++)
+            {
+                for(int k=artifacts[i][1];k<=artifacts[i][3];k++)
+                {
+                   if(grid[j][k]!=-1){
+                       ok=false;
+                       break;
+                   }
+                       
                 }
             }
-            if (good) ++ans;
+            if(ok==true)
+                cnt++;
         }
-        return ans;
+        return cnt;
+        
     }
 };

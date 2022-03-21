058bc9fa002f46bde3ca5e7246df8ecdba18a002
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-03-21 12:50:40 +0530

    Time: 63 ms (12.65%), Space: 11.1 MB (23.61%) - LeetHub

---
+++ 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long countVowels(string word) {
+        long long ans=0;
+        for(int i=0;i<word.size();i++)
+        {
+                if(string("aeiou").find(word[i])!=string::npos)
+                    ans+=(i+1)*(word.size()-i);
+            
+        }
+        return ans;
+    }
+};

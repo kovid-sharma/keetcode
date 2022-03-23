7a5b7702577643456eb3fb676e5db66cec581aa7
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-03-23 09:53:35 +0530

    Time: 3 ms, Memory: 8.8 MB - LeetHub

---
+++ remove-element/remove-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int l=nums.size()-1;
+        int i=0;
+        while(i<=l){
+            if(nums[i]==val){
+                nums[i]=nums[l--];
+            }
+            else
+                i++;
+                
+        }
+        return l+1;
+    }
+};

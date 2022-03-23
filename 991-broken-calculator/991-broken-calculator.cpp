890dce0adbdc084d3ef2fbb9cfbea3a104d31ca0
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-03-23 11:04:44 +0530

    Time: 480 ms (6.25%), Space: 5.8 MB (95.31%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -2,10 +2,6 @@ class Solution {
 public:
     int brokenCalc(int startValue, int target) {
         int cnt=0;
-        while(target<startValue){
-            cnt++;
-            target++;
-        }
         while(target>startValue){
             if(target%2==1){
                 target++;

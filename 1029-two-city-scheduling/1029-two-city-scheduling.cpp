3916b51debd3544ebbd45b09b35b3a337d87f794
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-03-25 22:19:09 +0530

    Time: 12 ms (23.24%), Space: 10.1 MB (7.87%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,20 +1,15 @@
+bool mycomp(vector<int>a,vector<int>b){
+        return (a[1]-a[0])>(b[1]-b[0]);
+    }
 class Solution {
 public:
-   static bool mycomp(vector<int>a,vector<int>b){
-        int asave= a[1]-a[0];
-        int bsave= b[1]-b[0];
-        return asave>bsave;
-        
-    }
     int twoCitySchedCost(vector<vector<int>>& costs) {
         sort(costs.begin(),costs.end(),mycomp);
         int n= costs.size()/2;
         int ans=0;
         for(int i=0;i<n;i++){
-            ans+=(costs[i][0]);
-        }
-        for(int i=n;i<2*n;i++){
-            ans+=(costs[i][1]);
+            ans+=costs[i][0];
+            ans+=costs[i+n][1];
         }
         return ans;
     }

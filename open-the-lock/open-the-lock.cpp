47404e3655f16cc364633dbf7e780142f2a649c0
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-03-29 21:20:45 +0530

    Time: 350 ms, Memory: 81.9 MB - LeetHub

---
+++ open-the-lock/open-the-lock.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<string> turner(int idx,string tmp)
+    {
+        vector<string> res(2, tmp);
+	    res[0][idx] = '0' + (res[0][idx] - '0' + 1) % 10;        // forward turn
+	    res[1][idx] = '0' + (res[1][idx] - '0' - 1 + 10) % 10;   // backward turn
+	     return res;
+    }
+    int openLock(vector<string>& deadends, string target) {
+        queue<string>bfs;
+        string init="0000";
+        bfs.push(init);
+        unordered_set<string>visited;
+        //unordered_set<string>deadp;
+        for(auto s:deadends){
+            visited.insert(s);
+            if(s==init)return -1;
+        }
+        visited.insert(init);
+        if(target==init)return 0;
+        int ans=0;
+        while(!bfs.empty())
+        {
+            int n=bfs.size();
+            ans++;
+            for(int i=0;i<n;i++)
+            {
+                string tmp= bfs.front();
+                bfs.pop();
+                for(int j=0;j<4;j++)
+                {
+                    for(auto str: turner(j,tmp))
+                    {
+                        if(/*deadp.find(str)==deadp.end() and */visited.find(str)==visited.end())
+                        {
+                            if(target==str)return ans;
+                            else{
+                                bfs.push(str);
+                                visited.insert(str);
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+        return -1;
+    }
+};

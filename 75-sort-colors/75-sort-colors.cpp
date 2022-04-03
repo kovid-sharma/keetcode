974967fc0a74579ca0c977f04e3513bfe5f421f8
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-04-03 22:26:14 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (24.41%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        //sort of selection sort
-        //choose three pointer and 1 value is where it should be only 2 and 0 are misplaced 
-        int p_zero=0,p_one=0,p_two=nums.size()-1;
-        if(p_two==0)return;
-        while(p_one<=p_two)
+        int l=0;
+        int r=nums.size()-1;
+        while(r>=l)
         {
-            if(nums[p_one]==1)
-                p_one++;
-            else if(nums[p_one]==0)
-            {   
-                //inplace swap without new int
-                nums[p_zero]+=nums[p_one];
-                nums[p_one]=nums[p_zero]-nums[p_one];
-                nums[p_zero]-=nums[p_one];
-                p_one++;
-                p_zero++;
+            if(nums[r]==0 and nums[l]!=0){
+                swap(nums[r],nums[l]);
+                l++;
+                r--;
             }
-            else    //nums[p_one]must be 2;
-            {   
-                //inplace swap 
-                nums[p_one]=nums[p_two];
-                nums[p_two]=2;
-                p_two--;
+            else if(nums[r]==0 and nums[l]==0)
+                l++;
+            else
+                r--;
+        }
+        int left;
+        for(int i=0;i<nums.size();i++)
+            if(nums[i]!=0)
+            {
+                left=i;
+                break;
             }
+        int rght=nums.size()-1;
+        while(rght>=left)
+        {
+            if(nums[rght]==1 and nums[left]==2)
+            {
+                swap(nums[left],nums[rght]);
+                left++;
+                rght--;
+            }
+            else if(nums[rght]==1 and nums[left]==1)
+                left++;
+            else
+                rght--;
         }
     }
 };

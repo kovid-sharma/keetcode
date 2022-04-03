86edbdc06947df736776aee1a778bf685bbae49d
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-04-03 09:31:30 +0530

    Time: 4 ms (73.50%), Space: 12.2 MB (37.25%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -2,6 +2,34 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         if(nums.size()==1)return;
+        int n=nums.size();
+        int to_swap;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+            {
+                to_swap=i;break;
+            }
+            if(i==0)
+            {
+                reverse(nums.begin(),nums.end());return;
+            }
+            
+        }
+        int to_swap_2;
+        for(int i=n-1;i>to_swap;i--)
+        {
+            if(nums[i]>nums[to_swap]){
+                to_swap_2=i;break;
+            }
+        }
+        swap(nums[to_swap],nums[to_swap_2]);
+        reverse(nums.begin()+to_swap+1,nums.end());
+        
+    }
+};
+/*void nextPermutation(vector<int>& nums) {
+        if(nums.size()==1)return;
        int leftmost;
         int i;
         for(i=nums.size()-2;i>=0;i--)
@@ -28,5 +56,4 @@ public:
         swap(nums[leftmost],nums[rightofleft]);
         reverse(nums.begin()+leftmost+1,nums.end());
         
-    }
-};
+    }*/

0ed9c127ea0eb4f4741330de0d31fc609e8dbb0b
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-04-03 21:40:13 +0530

    Time: 0 ms (100.00%), Space: 12.1 MB (37.25%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,59 +1,24 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        if(nums.size()==1)return;
         int n=nums.size();
-        int to_swap;
+        if(n==1)return;
+        int left;
         for(int i=n-2;i>=0;i--)
         {
-            if(nums[i]<nums[i+1])
-            {
-                to_swap=i;break;
-            }
-            if(i==0)
-            {
-                reverse(nums.begin(),nums.end());return;
-            }
-            
+            if(nums[i]<nums[i+1]){
+                left=i;break;}
+            if(i==0){
+                reverse(nums.begin(),nums.end());return;}
         }
-        int to_swap_2;
-        for(int i=n-1;i>to_swap;i--)
+        int rght;
+        for(int i=n-1;i>left;i--)
         {
-            if(nums[i]>nums[to_swap]){
-                to_swap_2=i;break;
+            if(nums[i]>nums[left]){
+                rght=i;break;
             }
         }
-        swap(nums[to_swap],nums[to_swap_2]);
-        reverse(nums.begin()+to_swap+1,nums.end());
-        
+        swap(nums[left],nums[rght]);
+        reverse(nums.begin()+left+1,nums.end());
     }
 };
-/*void nextPermutation(vector<int>& nums) {
-        if(nums.size()==1)return;
-       int leftmost;
-        int i;
-        for(i=nums.size()-2;i>=0;i--)
-        {
-            if(nums[i]<nums[i+1])
-            {
-                leftmost=i;
-                break;
-            }
-            if(i==0)
-            {
-                reverse(nums.begin(),nums.end());return;
-            }
-        }
-        
-        int rightofleft;
-        for(i=nums.size()-1;i>leftmost;i--)
-        {
-            if(nums[i]>nums[leftmost]){
-                rightofleft=i;
-                break;
-            }
-        }
-        swap(nums[leftmost],nums[rightofleft]);
-        reverse(nums.begin()+leftmost+1,nums.end());
-        
-    }*/

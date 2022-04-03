112e28914eff04e4dcfd679d09f0cdf24221aa68
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-04-03 17:50:23 +0530

    Time: 23 ms (37.82%), Space: 13.1 MB (95.73%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -3,20 +3,27 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int row=matrix.size();
         int col=matrix[0].size();
-        vector<int>row_idx(row,-1);
-        vector<int>col_idx(col,-1);
+        bool r=true;
+        bool c=true;
         for(int i=0;i<row;i++)
             for(int j=0;j<col;j++)
                 if(matrix[i][j]==0)
-                {
-                    row_idx[i]=0;
-                    col_idx[j]=0;
-                }
-        for(int i=0;i<row;i++)
-            for(int j=0;j<col;j++)
-                if(row_idx[i]==0 or col_idx[j]==0)
+                {   
+                    if(i==0)r=false;
+                    if(j==0)c=false;
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                } 
+        for(int i=1;i<row;i++)
+            for(int j=1;j<col;j++)
+                if(matrix[i][0]==0 or matrix[0][j]==0)
                     matrix[i][j]=0;
-        
+        if(!c)
+            for(int j=0;j<row;j++)
+                matrix[j][0]=0;
+        if(!r)
+            for(int j=0;j<col;j++)
+                matrix[0][j]=0;
             
     }
 };

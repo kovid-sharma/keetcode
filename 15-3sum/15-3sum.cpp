d16c459ed9c30712e489f48050ab72d067a27e64
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-04-06 13:05:57 +0530

    Time: 79 ms (87.21%), Space: 21.2 MB (39.95%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
         int n=nums.size();
-        vector<vector<int>> ret;
-        for(int i=0;i<n-2;i++)
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++)
         {
-            if(nums[i]>0)break;
-            if(i>0 and nums[i]==nums[i-1])continue;
-            int j=i+1,k=n-1;
-            while(j<k)
+            int target= -1*(nums[i]);
+            int st=i+1;
+            int en=n-1;
+            while(st<en)
             {
-                int sum= nums[i]+nums[j]+nums[k];
-                if(sum>0) k--;
-                else if(sum<0) j++;
+                int sum= nums[st]+nums[en];
+                if(sum<target)
+                    st++;
+                else if(sum>target)
+                    en--;
                 else
                 {
-                   ret.push_back(vector<int>{nums[i],nums[j],nums[k]});
-                    while(j+1<k and nums[j]==nums[j+1]) j++;
-                    while(k-1>j and nums[k]==nums[k-1]) k--;
-                    j++;k--;
+                    vector<int>now(3);
+                    now[0]=nums[i];
+                    now[1]=nums[st];
+                    now[2]=nums[en];
+                    ans.push_back(now);
+                    while(st<en and nums[st]==now[1])
+                        st++;
+                    while(st<en and nums[en]==now[2])
+                        en--;
                 }
             }
+            while(i+1<n and nums[i]==nums[i+1])
+                i++;
         }
-        return ret;
+        return ans;
     }
 };
-

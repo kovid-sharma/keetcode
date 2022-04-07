c9107353bad795d92ef249a9798df8057715bfcf
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-04-08 00:15:08 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (19.66%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -5,12 +5,13 @@ public:
         if(n==0)return 0;
         if(n==1)return 1;
         if(n==2)return 1;
-        if(dp[n]!=-1)
-            return dp[n];
-        return dp[n]=rec(n-1,dp)+rec(n-2,dp)+rec(n-3,dp);
+        if(dp[n-2]!=-1)
+            return dp[n-2];
+        return dp[n-2]=rec(n-1,dp)+rec(n-2,dp)+rec(n-3,dp);
     }
     int tribonacci(int n) {
-        vector<int>dp(n+1,-1);
+        if(n==0)return 0;
+        vector<int>dp(n-1,-1);
         return rec(n,dp);
     }
 };

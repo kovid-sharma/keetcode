057bde4355d8812f4041b5a3c7ea2d937f6327f5
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-04-07 12:00:17 +0530

    Time: 3 ms (35.41%), Space: 6.6 MB (25.74%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -3,16 +3,16 @@ public:
     
     int rec(int m,int n,vector<vector<int>>&dp)
     {  
-        if(dp[m][n]!=-1)
+        if(dp[m][n]!=0)
             return dp[m][n];
         if(m==0 or n==0)
-            return dp[m][n]=0;
+            return 0;
         if(m==1 and n==1)
-            return dp[m][n]=1;
+            return 1;
         return dp[m][n]=rec(m-1,n,dp)+rec(m,n-1,dp);
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
         return rec(m,n,dp);
     }
 };

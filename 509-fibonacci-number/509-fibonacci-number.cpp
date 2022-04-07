4c492f8e7bff64ba5ae0759c0bf7af44dd23ed47
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-04-07 12:48:50 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (10.03%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rec(int n,vector<int>&dp)
+    {
+        if(n<=1)
+            return n;
+        if(dp[n]!=-1)
+            return dp[n];
+        return dp[n]=rec(n-1,dp)+rec(n-2,dp);
+    }
+    int fib(int n) {
+       vector<int>dp(n+1,-1);
+        return rec(n,dp);
+    }
+};

6d9ab4f32c11b2ee1c50f0d952282b9d025f152a
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-04-07 21:15:33 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (33.27%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -4,19 +4,23 @@ public:
         vector<vector<int>>ans;
         ans.push_back({1});
         if(numRows==1)return ans;
+        if(numRows==2){
+            ans.push_back({1,1});return ans;
+        }
         ans.push_back({1,1});
         for(int i=3;i<=numRows;i++)
         {
-            vector<int>tmp;
-            tmp.push_back(1);
-            for(int j=0;j<i-2;j++)
+            vector<int>row;
+            row.push_back(1);
+            for(int j=1;j<=i-2;j++)
             {
-                vector<int>now=ans.back();
-                int add= now[j]+now[j+1];
-                tmp.push_back(add);
+                vector<int>lastrow=ans.back();
+                int sum=lastrow[j]+lastrow[j-1];
+                row.push_back(sum);
             }
-            tmp.push_back(1);  
-            ans.push_back(tmp);
+            row.push_back(1);
+            ans.push_back(row);
+            row.clear();
         }
         return ans;
     }

d691243cef7787493ad16b95e0e55e9ce5c11d59
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-04-07 21:06:08 +0530

    Time: 3 ms (39.89%), Space: 6.3 MB (9.62%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
+    bool rec(int n,vector<int>&dp)
+    {
+        if(n==1)return false;
+        if(n==2)return true;
+        if(dp[n]!=-1)
+            return dp[n];
+        return dp[n]=!rec(n-1,dp);
+    }
     bool divisorGame(int n) {
-        if(n==2)
-            return true;
-        if(n==1)
-            return false;
-        return !divisorGame(n-1);
+        vector<int>dp(n+1,-1);
+        return rec(n,dp);
     }
 };

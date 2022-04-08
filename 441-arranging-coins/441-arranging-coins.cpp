6fe4c7b8c07648b9332db950ef858aba8c52d7fe
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-04-08 15:45:29 +0530

    Time: 3 ms (81.40%), Space: 5.8 MB (72.65%) - LeetHub

---
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        if(n==1)return 1;
+        if(n==3)return 2;
+        int lo=1;
+        int hi=n/2;
+        while(lo<=hi)
+        {
+            long mid=lo+(hi-lo)/2;
+            long bana= (mid)*(mid+1)/2;
+            if(bana==n)
+                return mid;
+            else if(bana>n)
+                hi=mid-1;
+            else
+                lo=mid+1;
+        }
+        return hi;
+    }
+};

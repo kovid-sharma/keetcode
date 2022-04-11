c16900b3c6c64eca8df31d49b212aff1da62c050
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-04-11 09:24:19 +0530

    Time: 45 ms (47.86%), Space: 13.8 MB (97.44%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -5,22 +5,11 @@ public:
         int n=grid[0].size();
         if(k==0)return grid;
         vector<vector<int>>ans(m,vector<int>(n,0));
-        while(k--){
-        for(int i=0;i<m;i++)
+        for(int i = 0;i<n*m; i++)
         {
-            for(int j=0;j<n-1;j++)
-            {
-                ans[i][j+1]=grid[i][j];
-            }
-        }
-        ans[0][0]=grid[m-1][n-1];
-        for(int i=0;i<m-1;i++)
-        {
-            ans[i+1][0]=grid[i][n-1];
-        }
-        grid=ans;
+             int temp = (i+k)%(n*m);
+             ans[temp/n][temp%n] = grid[i/n][i%n];
         }
         return ans;
-        
     }
 };

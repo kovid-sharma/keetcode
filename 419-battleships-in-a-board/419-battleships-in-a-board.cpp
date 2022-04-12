48a2da52cedfe417d9cf62a2455e962ab84e96bc
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-04-12 18:19:10 +0530

    Time: 12 ms (38.36%), Space: 8.2 MB (51.25%) - LeetHub

---
+++ 419-battleships-in-a-board/419-battleships-in-a-board.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void dfs(int row,int col,int x,int y,vector<vector<char>>& board)
+    {   
+        if(x<0 or y<0 or x>=row or y>=col)return;
+        if(board[x][y]=='X')
+        {
+            board[x][y]=':';
+            dfs(row,col,x+1,y,board);
+            dfs(row,col,x-1,y,board);
+            dfs(row,col,x,y+1,board);
+            dfs(row,col,x,y-1,board);
+        }
+        else
+            return;
+    }
+    int countBattleships(vector<vector<char>>& board) {
+        int cnt=0;
+        int row=board.size();
+        int col=board[0].size();
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(board[i][j]=='X')
+                {
+                    cnt++;
+                    dfs(row,col,i,j,board);
+                }
+            }
+        }
+        return cnt;
+    }
+};

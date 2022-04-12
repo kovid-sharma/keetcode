a9d5449fa3a025374c0c09604c634689fd924980
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-04-12 10:16:53 +0530

    Time: 0 ms (100.00%), Space: 7 MB (58.55%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -1,38 +1,50 @@
 class Solution {
 public:
-    void gameOfLife(vector<vector<int>>& bo) {
-        //  State transitions
-       //  0 : dead to dead
-      //  1 : live to live
-     //  2 : live to dead
-    //  3 : dead to live
-        int row=bo.size();
-        int col=bo[0].size();
+    void gameOfLife(vector<vector<int>>& board) {
+        // 0 dead to dead
+        // 1 live to live
+        // 2 live to dead
+        // 3 dead to live
+        int m=board.size();
+        int n=board[0].size();
         int di[8] = {-1, -1, -1,  0, 0,  1, 1, 1};
         int dj[8] = {-1,  0,  1, -1, 1, -1, 0, 1};
-        for(int i=0;i<row;i++)
-            for(int j=0;j<col;j++)
+        for(int i=0;i<m;i++)
+        {   
+            
+            for(int j=0;j<n;j++)
             {   
+                
                 int cnt_live=0;
                 for(int k=0;k<8;k++)
                 {
                     int i_new=i+di[k];
                     int j_new=j+dj[k];
-                    if(i_new<0 or j_new<0 or i_new>=row or j_new>=col)
+                    if(i_new<0 or j_new<0 or i_new>=m or j_new>=n)
                         continue;
-                    if(bo[i_new][j_new]==1 or bo[i_new][j_new]==2)
+                    if(board[i_new][j_new]==1 or board[i_new][j_new]==2)
                         cnt_live++;
                     
                 }
-                if(bo[i][j]==1 and cnt_live>3)
-                    bo[i][j]=2;
-                if(bo[i][j]==1 and cnt_live<2)
-                    bo[i][j]=2;
-                if(bo[i][j]==0 and cnt_live==3)
-                    bo[i][j]=3;
+                if(board[i][j]==1)
+                {   
+                    if(cnt_live>3)
+                    board[i][j]=2;
+                    else if(cnt_live==2 or cnt_live==3)
+                    board[i][j]=1;
+                    else if(cnt_live<2)
+                        board[i][j]=2;
+                }
+                else if(board[i][j]==0)
+                {
+                    if(cnt_live==3)
+                        board[i][j]=3;
+                }
             }
-        for(int i=0;i<row;i++)
-            for(int j=0;j<col;j++)
-                bo[i][j]%=2;
+        }
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
+                board[i][j]%=2;
+        
     }
 };

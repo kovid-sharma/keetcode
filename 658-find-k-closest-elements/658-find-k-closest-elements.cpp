892b5ee8c3c4a37e7104243365c5a7fa7a0df1a0
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-04-16 09:31:58 +0530

    Time: 43 ms (86.49%), Space: 31.8 MB (46.67%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,23 +1,22 @@
-int toc;
 class Solution {
 public:
-    
-    static bool mycomp(int a,int b)
-    {
-        if(abs(a-toc)==abs(b-toc))
-            return a<b;
-        return abs(a-toc)<abs(b-toc);
-    }
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        toc=x;
-        sort(arr.begin(),arr.end(),mycomp);
-        vector<int>ans;
-        int i=0;
-        while(k--){
-            ans.push_back(arr[i]);
-            i++;
+        int n=arr.size();
+        int lo=0;
+        int hi=n-1;
+        int sz=hi-lo+1;
+        while(sz>k)
+        {
+            if(abs(arr[lo]-x)>abs(arr[hi]-x))
+                lo++;
+            else
+                hi--;
+            sz=hi-lo+1;
         }
-        sort(ans.begin(),ans.end());
-        return ans;
+        vector<int>ans;
+        for(int i=lo;i<=hi;i++)
+        {
+            ans.push_back(arr[i]);
+        }return ans;
     }
 };

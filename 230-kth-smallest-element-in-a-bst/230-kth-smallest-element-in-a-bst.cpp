3cbaee3c7c6d9eeba02b3e65cfcbbb3f78ed6e5e
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-04-18 09:17:09 +0530

    Time: 29 ms (33.18%), Space: 24.1 MB (60.91%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -1,39 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
+    int ans;
+    int cnt=0;
+    void traver(TreeNode*root,int k)
+    {
+        if(root==NULL)return;
+        traver(root->left,k);
+        if(++cnt==k)ans=root->val;
+        traver(root->right,k);
+    }
     int kthSmallest(TreeNode* root, int k) {
-        priority_queue<int>pq;
-        TreeNode*st=root;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty())
-        {
-            int n=q.size();
-            for(int i=0;i<n;i++)
-            {
-                TreeNode*tmp=q.front();
-                q.pop();
-                pq.push(tmp->val*-1);
-                if(tmp->right)
-                    q.push(tmp->right);
-                if(tmp->left)
-                    q.push(tmp->left);
-            }
-        }
-        while(--k)
-        {
-            pq.pop();
-        }
-        return -1*pq.top();
+        traver(root,k);
+        return ans;
     }
 };

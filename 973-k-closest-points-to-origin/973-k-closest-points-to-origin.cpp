e093d1cb98ed3f8568a99161b3e64f171e0bf31e
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-04-21 09:04:38 +0530

    Time: 300 ms (56.67%), Space: 65.5 MB (54.12%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    static bool mycomp(tuple<int,int,int>&a,tuple<int,int,int>&b)
-    {
-        return get<2>(a)<get<2>(b);
-    }
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<tuple<int,int,int>>x1x2sum;
+        priority_queue<pair<int,pair<int,int>>>pq;
         for(int i=0;i<points.size();i++)
         {
-            int x1= points[i][0];
-            int x2= points[i][1];
-            int sum= (x1*x1)+(x2*x2);
-            x1x2sum.push_back(make_tuple(x1,x2,sum));
+            int a= points[i][0];
+            int b=points[i][1];
+            int add= a*a + b*b;
+            pair<int,pair<int,int>> topush;
+            topush.first=-1*add;
+            topush.second.first=a;
+            topush.second.second=b;
+            pq.push(topush);
         }
-        sort(x1x2sum.begin(),x1x2sum.end(),mycomp);
         vector<vector<int>>ans;
-        int i=0;
         while(k--)
         {
-            ans.push_back({get<0>(x1x2sum[i]),get<1>(x1x2sum[i])});
-            i++;
-        }
-        return ans;
+            ans.push_back({pq.top().second.first,pq.top().second.second});
+            pq.pop();
+        }return ans;
     }
 };

a8104141148d20937e2fdf08049746990c9a3f2c
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-04-29 14:34:01 +0530

    Time: 10 ms (73.12%), Space: 10.3 MB (93.25%) - LeetHub

---
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -0,0 +1,153 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n=rooms.size();
+        vector<int>milgyi(n,0);
+        milgyi[0]=1;
+        for(int i=0;i<n;i++)
+        {
+            if(milgyi[i]==1)
+            {
+                for(int j=0;j<rooms[i].size();j++)
+                {
+                    milgyi[rooms[i][j]]=1;
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(milgyi[i]==1)
+            {
+                for(int j=0;j<rooms[i].size();j++)
+                {
+                    milgyi[rooms[i][j]]=1;
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(milgyi[i]==1)
+            {
+                for(int j=0;j<rooms[i].size();j++)
+                {
+                    milgyi[rooms[i][j]]=1;
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(milgyi[i]==1)
+            {
+                for(int j=0;j<rooms[i].size();j++)
+                {
+                    milgyi[rooms[i][j]]=1;
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(milgyi[i]==1)
+            {
+                for(int j=0;j<rooms[i].size();j++)
+                {
+                    milgyi[rooms[i][j]]=1;
+                }
+            }
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            if(milgyi[i]==1)
+            {
+                for(int j=0;j<rooms[i].size();j++)
+                {
+                    milgyi[rooms[i][j]]=1;
+                }
+            }
+        }
+    
+         for(int i=0;i<n;i++)
+        {
+            if(milgyi[i]==1)
+            {
+                for(int j=0;j<rooms[i].size();j++)
+                {
+                    milgyi[rooms[i][j]]=1;
+                }
+            }
+        }
+         for(int i=0;i<n;i++)
+        {
+            if(milgyi[i]==1)
+            {
+                for(int j=0;j<rooms[i].size();j++)
+                {
+                    milgyi[rooms[i][j]]=1;
+                }
+            }
+        }
+         for(int i=0;i<n;i++)
+        {
+            if(milgyi[i]==1)
+            {
+                for(int j=0;j<rooms[i].size();j++)
+                {
+                    milgyi[rooms[i][j]]=1;
+                }
+            }
+        }
+         for(int i=0;i<n;i++)
+        {
+            if(milgyi[i]==1)
+            {
+                for(int j=0;j<rooms[i].size();j++)
+                {
+                    milgyi[rooms[i][j]]=1;
+                }
+            }
+        }
+         for(int i=0;i<n;i++)
+        {
+            if(milgyi[i]==1)
+            {
+                for(int j=0;j<rooms[i].size();j++)
+                {
+                    milgyi[rooms[i][j]]=1;
+                }
+            }
+        }
+         for(int i=0;i<n;i++)
+        {
+            if(milgyi[i]==1)
+            {
+                for(int j=0;j<rooms[i].size();j++)
+                {
+                    milgyi[rooms[i][j]]=1;
+                }
+            }
+        }
+         for(int i=0;i<n;i++)
+        {
+            if(milgyi[i]==1)
+            {
+                for(int j=0;j<rooms[i].size();j++)
+                {
+                    milgyi[rooms[i][j]]=1;
+                }
+            }
+        }
+         for(int i=0;i<n;i++)
+        {
+            if(milgyi[i]==1)
+            {
+                for(int j=0;j<rooms[i].size();j++)
+                {
+                    milgyi[rooms[i][j]]=1;
+                }
+            }
+        }
+        
+        int sum= accumulate(milgyi.begin(),milgyi.end(),0);
+        return sum==n;
+    }
+};

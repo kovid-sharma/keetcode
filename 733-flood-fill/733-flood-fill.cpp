7f6089367ba0ca278946f973fb7e30eea0bc4baa
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-04-29 20:03:12 +0530

    Time: 16 ms (32.82%), Space: 14.1 MB (51.45%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
+    int row,col,nya,paint;
+    void dfsfill(int idx,int idy,vector<vector<int>>&image,vector<vector<bool>>&visited)
+    {
+        if(idx<0 or idy<0 or idx>=row or idy>=col or visited[idx][idy]==true or image[idx][idy]!=paint)
+            return;
+        
+        
+        visited[idx][idy]=true;
+        image[idx][idy]=nya;
+        dfsfill(idx+1,idy,image,visited);
+        dfsfill(idx-1,idy,image,visited);
+         dfsfill(idx,idy+1,image,visited);
+         dfsfill(idx,idy-1,image,visited);
+    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        //bfs easy
-        int coltosee=image[sr][sc];
-        if(newColor==image[sr][sc])
-            return image;
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
-        vector<int>dir={0,-1,0,1,0};
-        while(!q.empty())
-        {
-            pair<int,int>now=q.front();
-            q.pop();
-            image[now.first][now.second]=newColor;
-            for(int i=0;i<4;i++)
-            {
-                int xpos=now.first+dir[i];
-                int ypos=now.second+dir[i+1];
-                if(xpos<0 or ypos<0 or xpos>=image.size() or ypos>=image[0].size())
-                    continue;
-                if(image[xpos][ypos]==coltosee)
-                {
-                    q.push({xpos,ypos});
-                }
-            }
-        }
+        row=image.size();
+        col=image[0].size();
+        paint=image[sr][sc];
+        nya=newColor;
+        vector<vector<bool>>visited(row,vector<bool>(col,false));
+        dfsfill(sr,sc,image,visited);
         return image;
     }
 };

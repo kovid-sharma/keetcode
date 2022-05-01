975f31ba9b14a0267983dd75410494eeeb9e0657
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-05-01 07:49:33 +0530

    Time: 27 ms (51.80%), Space: 23.1 MB (89.29%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -3,9 +3,8 @@ public:
     int a(vector<vector<int>>&grid,int idx,int idy){
         if(idx<0 or idy<0 or idx>=grid.size() or idy>=grid[0].size() or grid[idx][idy]==0)
             return 0;
-        if(grid[idx][idy]==2)
-            return 0;
-        grid[idx][idy]=2;
+        
+        grid[idx][idy]=0;
         return 1+
         a(grid,idx+1,idy)+
         a(grid,idx-1,idy)+

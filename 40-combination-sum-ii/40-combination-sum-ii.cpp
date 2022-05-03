bf07254cb28beec12b533fc853b16bc717f476ea
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-05-03 21:43:04 +0530

    Time: 17 ms (23.92%), Space: 12.9 MB (24.78%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,39 +1,37 @@
 class Solution {
-public:
-    vector<vector<int>>ans;
-    void rec(vector<int>& candidates, int target,vector<int>&now,int idx){
-        if(target==0){
-            ans.push_back(now);
+public:  
+    void solve(vector<vector<int>>&unqCom,vector<int>&pickOrNot,int idx,int target,vector<int>&c)
+    {
+        
+        if(target<0)
             return;
+        if(target==0){
+            unqCom.push_back(pickOrNot);return;
         }
-        if(target<0)return;
-        if(idx==candidates.size())return;
-        //take
-        int currval=candidates[idx];
-        now.push_back(currval);
-        rec(candidates,target-currval,now,idx+1);
-        now.pop_back();
-        idx++;
-        //dont take
-        while(idx<candidates.size() and candidates[idx]==currval)idx++;
-        rec(candidates,target,now,idx);
+        if(idx==c.size())
+            return;
+        
+        
+        //let's pick element
+        int currEle=c[idx];
+        pickOrNot.push_back(currEle);
+        
+        solve(unqCom,pickOrNot,idx+1,target-currEle,c);
+        //We don't care if target went on to be 0
+        //We backtrack to implement NOT pick case
+        pickOrNot.pop_back();
+        
+        while(idx<c.size() and c[idx]==currEle)idx++;
+        //Let's NOT pick
+        solve(unqCom,pickOrNot,idx,target,c);
         
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<int>now;
+        vector<vector<int>>unqCom;
+        vector<int>pickOrNot;
+        int idx=0;//starting index of choices
         sort(candidates.begin(),candidates.end());
-        rec(candidates,target,now,0);
-        
-        return ans;
+        solve(unqCom,pickOrNot,idx,target,candidates);
+        return unqCom;
     }
 };
-
-
-
-   
-   
-   
-   
-   /* while (A[pos] == num) ++pos;
-    combinationSum2(A, pos, path, target, res);
-}*/

e67916c765b57299d170a89f9e0143436c267bda
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-05-05 09:08:29 +0530

    Time: 10 ms (73.52%), Space: 10.2 MB (93.14%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,89 +1,22 @@
 class Solution {
 public:
+    void dfs(int idx,vector<bool>&vis,vector<vector<int>>&rooms){
+        for(int i=0;i<rooms[idx].size();i++)
+            if(vis[rooms[idx][i]]==false){
+                vis[rooms[idx][i]]=true;
+                dfs(rooms[idx][i],vis,rooms);
+                }
+    }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n=rooms.size();
-        vector<int>milgyi(n,0);
-        milgyi[0]=1;
-        for(int i=0;i<n;i++)
-        {
-            if(milgyi[i]==1)
-            {
-                for(int j=0;j<rooms[i].size();j++)
-                {
-                    milgyi[rooms[i][j]]=1;
-                }
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(milgyi[i]==1)
-            {
-                for(int j=0;j<rooms[i].size();j++)
-                {
-                    milgyi[rooms[i][j]]=1;
-                }
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(milgyi[i]==1)
-            {
-                for(int j=0;j<rooms[i].size();j++)
-                {
-                    milgyi[rooms[i][j]]=1;
-                }
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(milgyi[i]==1)
-            {
-                for(int j=0;j<rooms[i].size();j++)
-                {
-                    milgyi[rooms[i][j]]=1;
-                }
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(milgyi[i]==1)
-            {
-                for(int j=0;j<rooms[i].size();j++)
-                {
-                    milgyi[rooms[i][j]]=1;
-                }
-            }
-        }
+        vector<bool>vis(n,false);
+        vis[0]=true;
+        dfs(0,vis,rooms);
+        bool hit=true;
+        for(int i=0;i<vis.size();i++)
+            if(vis[i]==false)
+                hit=false;
+        return hit;
         
-        for(int i=0;i<n;i++)
-        {
-            if(milgyi[i]==1)
-            {
-                for(int j=0;j<rooms[i].size();j++)
-                {
-                    milgyi[rooms[i][j]]=1;
-                }
-            }
-        }
-     for(int i=0;i<n;i++)
-        {
-            if(milgyi[i]==1)
-            {
-                for(int j=0;j<rooms[i].size();j++)
-                {
-                    milgyi[rooms[i][j]]=1;
-                }
-            }
-        }
-         
-        
-        
-        
-        
-         
-         
-        
-        int sum= accumulate(milgyi.begin(),milgyi.end(),0);
-        return sum==n;
     }
 };

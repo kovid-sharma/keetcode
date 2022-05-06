8eccfb170664b0bf521516b414a4709165be8ca5
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-05-06 10:45:41 +0530

    Time: 73 ms (17.14%), Space: 10.2 MB (70.45%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -6,29 +6,20 @@ public:
         for(int i=0;i<s.size();i++)
         {
             if(!st.empty() and s[st.top().first]==s[i])
-            {
                 st.top().second++;
-                if(st.top().second==k)
-                    st.pop();
-            }
             else
-            {
                 st.push({i,1});
-                if(st.top().second==k)//in caase k is actually 1
+            if(st.top().second==k)
                     st.pop();
-            }
         }
         string ret;
         while(!st.empty())
         {
             int num=st.top().second;
             for(int i=0;i<num;i++)
-            {
                 ret.push_back(s[st.top().first]);
-            }
             st.pop();
         }
-       
         reverse(ret.begin(),ret.end());
         return ret;
     }

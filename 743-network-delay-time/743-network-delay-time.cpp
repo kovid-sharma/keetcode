d791baa1aec7d5381e3538efd67472d8bdc26d83
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-05-14 18:27:11 +0530

    Time: 127 ms (83.79%), Space: 40 MB (63.30%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<int>disSt(n,INT_MAX);
-        disSt[k-1]=0;
-        int cp=n;
-        while(n--)
-        {   
-            bool see=false;
-            for(auto it:times)
+        unordered_map<int,vector<pair<int,int>>>graph;
+        for(int i=0;i<times.size();i++)
+            graph[times[i][0]].push_back({times[i][1],times[i][2]});
+        vector<int>dist(n+1,INT_MAX);
+        queue<int>q;
+        q.push(k);
+        dist[k]=0;
+        //bool seen=false;
+        while(!q.empty())
+        {
+            int fr=q.front();
+            q.pop();
+            for(auto it:graph[fr])
             {
-                int src=it[0];
-                int des=it[1];
-                int wei=it[2];
-                if(disSt[src-1]!=INT_MAX and disSt[src-1]+wei<disSt[des-1]){
-                    disSt[des-1]=disSt[src-1]+wei;
-                    see=true;
+                if(dist[it.first]>dist[fr]+it.second)
+                {
+                    dist[it.first]=dist[fr]+it.second;
+                    //seen=true;
+                    //ans=max(dist[it.first],ans);
+                     q.push(it.first);
                 }
+               
             }
-            if(see==false)
-                break;
         }
-        int maxWei=0;
-        for(int i=0;i<cp;i++)
-            maxWei=max(maxWei,disSt[i]);
-        if(maxWei==INT_MAX)
-            return -1;
-        return maxWei;
+        int res=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(dist[i]==INT_MAX)
+                return -1;
+            res=max(res,dist[i]);
+        }
+        return res;
     }
 };

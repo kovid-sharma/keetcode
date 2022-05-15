25dbefa49b4b7fe824c7787d4ba60ffe44703e36
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-05-15 21:39:53 +0530

    Time: 2 ms (46.01%), Space: 6.5 MB (61.32%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    string tmp="";
-    vector<string> what = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-    void solve(vector<string>&ans,int index,int sz,string digits)
+    vector<string>use;
+    void solve(int i,vector<string>&ans,string &dig,string &tmp)
     {
-        if(index==digits.size())
+        if(i==dig.size())
         {
             ans.push_back(tmp);
             return;
         }
-        
-        for(auto str:what[digits[index]-'2'])
+        for(auto ch:use[dig[i]-'2'])
         {
-            tmp.push_back(str);
-            solve(ans,index+1,sz,digits);
+            tmp.push_back(ch);
+            solve(i+1,ans,dig,tmp);
             tmp.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
         vector<string>ans;
-        if(digits.size()==0) return ans;
-        int sz=digits.size();
-        solve(ans,0,sz,digits);
+        if(digits.size()==0)
+            return ans;
+        string tmp;
+        
+        use.push_back("abc");
+         use.push_back("def");
+         use.push_back("ghi"); use.push_back("jkl"); use.push_back("mno");
+         use.push_back("pqrs");use.push_back("tuv");use.push_back("wxyz");
+        solve(0,ans,digits,tmp);
         return ans;
     }
 };

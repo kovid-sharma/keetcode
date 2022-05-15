95ff3a7d5bd4c97a0834a949c1a1ab26895cca47
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-05-15 13:13:08 +0530

    Added solution - LeetHub

---
+++ Unique Subsets - GFG/unique-subsets.cpp
@@ -0,0 +1,68 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find all possible unique subsets.
+    void solve(vector<int>&emp,vector<vector<int>>&ans,vector<int>&nums,int n,int i)
+    {
+        ans.push_back(emp);
+        for(int j=i;j<n;j++)
+        {
+            if(j>i and nums[j]==nums[j-1])
+            continue;
+            emp.push_back(nums[j]);
+            solve(emp,ans,nums,n,j+1);
+            emp.pop_back();
+        }
+        
+        
+    }
+    vector<vector<int> > AllSubsets(vector<int> arr, int n)
+    {
+        // code here 
+        sort(arr.begin(),arr.end());
+        vector<vector<int> >ans;
+        vector<int>emp;
+        solve(emp,ans,arr,n,0);
+        //sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> A;
+        int x;
+        for(int i=0;i<n;i++){
+            cin>>x;
+            A.push_back(x);
+        }
+        Solution obj;
+        vector<vector<int> > result = obj.AllSubsets(A,n);
+        // printing the output
+        for(int i=0;i<result.size();i++){
+            cout<<'(';
+            for(int j=0;j<result[i].size();j++){
+                cout<<result[i][j];
+                if(j<result[i].size()-1)
+                    cout<<" ";
+            }
+            cout<<")";
+        }
+        cout<<"\n";
+    }
+}   
+
+
+  // } Driver Code Ends

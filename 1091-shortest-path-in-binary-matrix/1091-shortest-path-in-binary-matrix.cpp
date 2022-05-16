3307f24621cc88fb92a8ae8410276ace3f46db8d
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-05-16 12:32:05 +0530

    Time: 67 ms (83.62%), Space: 19.2 MB (93.95%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -2,27 +2,28 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
+        //int col=grid[0].size();
         if(grid[0][0]==1 or grid[n-1][n-1]==1)
             return -1;
-        int move=1;
         queue<pair<int,int>>q;
         q.push({0,0});
-        grid[0][0]=1;
         int dir[8][2]={{1,0},{-1,0},{1,1},{-1,-1},{1,-1},{-1,1},{0,1},{0,-1}};
+        grid[0][0]=1;int sum=1;        
         while(!q.empty())
         {
-            int s=q.size();
-            for(int j=0;j<s;j++)
+            int sz=q.size();
+            while(sz--)
             {
-                int x=q.front().first;
-                int y=q.front().second;
+                auto fr=q.front();
+                int xid=fr.first;
+                int yid=fr.second;
                 q.pop();
-                if(x==n-1 and y==n-1)
-                    return move;
-                for(int i=0;i<8;i++)
+                if(xid==n-1 and yid==n-1)
+                    return sum;
+               for(int i=0;i<8;i++)
                 {
-                    int r=x+dir[i][0];
-                    int c=y+dir[i][1];
+                    int r=xid+dir[i][0];
+                    int c=yid+dir[i][1];
                     if(r>=0 and c>=0 and r<n and c<n and grid[r][c]==0){
                         grid[r][c]=1;
                         q.push({r,c});
@@ -30,7 +31,7 @@ public:
                 }
                 
             }
-            move++;
+            sum++;
         }
         return -1;
     }

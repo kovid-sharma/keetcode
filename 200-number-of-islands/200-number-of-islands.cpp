ddf2aec2d85f6cb505ab4b9a3745b27bd0f64414
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-05-18 08:08:34 +0530

    Time: 62 ms (24.88%), Space: 18.1 MB (18.03%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid,int row,int col,int i,int j){
-        if(i<0 or j<0 or i>=row or j>=col or grid[i][j]=='0')
-            return;
-
-            grid[i][j]='0';
-        dfs(grid,row,col,i+1,j);
-        dfs(grid,row,col,i-1,j);
-        dfs(grid,row,col,i,j-1);
-        dfs(grid,row,col,i,j+1);
-    }
     int numIslands(vector<vector<char>>& grid) {
-        //visisted can be made water
-        int row=grid.size();
-        int col=grid[0].size();
-        int numIsland=0;
-        for(int i=0;i<row;i++)
-            for(int j=0;j<col;j++)
+        int cnt=0;
+        vector<int>dir={0,-1,0,1,0};
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[0].size();j++)
+            {
                 if(grid[i][j]=='1')
                 {
-                    numIsland++;
-                    dfs(grid,row,col,i,j);
+                    cnt++;
+                    queue<pair<int,int>>q;
+                    q.push({i,j});
+                    grid[i][j]='0';
+                    while(!q.empty())
+                    {
+                        int sz=q.size();
+                        while(sz--)
+                        {   
+                            auto fr=q.front();
+                            q.pop();
+                            for(int di=0;di<4;di++)
+                            {
+                                int xN=fr.first+dir[di];
+                                int yN=fr.second+dir[di+1];
+                                if(xN>=0 and yN>=0 and xN<grid.size() and yN<grid[0].size() and grid[xN][yN]=='1')
+                                {
+                                    grid[xN][yN]='0';
+                                    q.push({xN,yN});
+                                }
+                            }
+                        }
+                    }
                 }
-        return numIsland;
+            }
+        }
+        return cnt;
     }
 };

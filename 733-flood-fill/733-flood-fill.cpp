f2c025e4d3988a9b811c81db92ac09170bf35f01
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-05-19 20:15:27 +0530

    Time: 28 ms (5.58%), Space: 14 MB (50.92%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    int row,col,nya,paint;
-    void dfsfill(int idx,int idy,vector<vector<int>>&image,vector<vector<bool>>&visited)
+    void dfs(vector<vector<int>>& image, int i, int j, int nr,int pr)
     {
-        if(idx<0 or idy<0 or idx>=row or idy>=col or visited[idx][idy]==true or image[idx][idy]!=paint)
+        if(i<0 or j<0 or i>=image.size() or j>=image[0].size() or image[i][j]==nr)
             return;
         
-        
-        visited[idx][idy]=true;
-        image[idx][idy]=nya;
-        dfsfill(idx+1,idy,image,visited);
-        dfsfill(idx-1,idy,image,visited);
-         dfsfill(idx,idy+1,image,visited);
-         dfsfill(idx,idy-1,image,visited);
+        if(image[i][j]==pr)
+        {   image[i][j]=nr;
+            dfs(image,i+1,j,nr,pr);
+            dfs(image,i,j-1,nr,pr);
+            dfs(image,i,j+1,nr,pr);
+            dfs(image,i-1,j,nr,pr);
+        }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        row=image.size();
-        col=image[0].size();
-        paint=image[sr][sc];
-        nya=newColor;
-        vector<vector<bool>>visited(row,vector<bool>(col,false));
-        dfsfill(sr,sc,image,visited);
+        dfs(image,sr,sc,newColor,image[sr][sc]);
         return image;
     }
 };

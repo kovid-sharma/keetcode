bd0ae435ddeb14a16c6f2cc4180aadb7fdd33814
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-05-19 08:57:16 +0530

    Time: 62 ms (55.34%), Space: 15.9 MB (71.48%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -14,7 +14,7 @@ public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int r=matrix.size();
         int c=matrix[0].size();
-        vector<vector<int>>dp(r+1,vector<int>(c+1,-1));
+        vector<vector<int>>dp(r,vector<int>(c,-1));
         int ans=0;
         for(int i=0;i<r;i++)
             for(int j=0;j<c;j++)

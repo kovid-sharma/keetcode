da50ee20b5420b2b115bfbb3c46cf962fbe9c843
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-05-19 08:56:46 +0530

    Time: 55 ms (69.58%), Space: 15.9 MB (71.48%) - LeetHub

---
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dfs(int r,int c,int i,int j,vector<vector<int>>& matrix,int prev,vector<vector<int>>&dp)
+    {
+        if(i<0 or j<0 or i>=r or j>=c or prev>=matrix[i][j])
+            return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int x1=dfs(r,c,i+1,j,matrix,matrix[i][j],dp);
+        int x2=dfs(r,c,i,j-1,matrix,matrix[i][j],dp);
+        int x3=dfs(r,c,i,j+1,matrix,matrix[i][j],dp);
+        int x4=dfs(r,c,i-1,j,matrix,matrix[i][j],dp);
+        return dp[i][j]=max({x1,x2,x3,x4})+1;
+    }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int r=matrix.size();
+        int c=matrix[0].size();
+        vector<vector<int>>dp(r+1,vector<int>(c+1,-1));
+        int ans=0;
+        for(int i=0;i<r;i++)
+            for(int j=0;j<c;j++)
+                ans=max(ans,dfs(r,c,i,j,matrix,-1,dp));
+        return ans;
+    }
+};

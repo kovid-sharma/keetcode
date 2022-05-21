79d2fe6537a76044b95ca2706b5a712056503835
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-05-21 13:13:17 +0530

    Time: 104 ms (62.23%), Space: 16.1 MB (29.48%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -3,22 +3,19 @@ public:
     int rec(vector<int>&dp,vector<int>&coins,int mon)
     {
         if(mon==0)
-            return 0;
-        if(dp[mon]!=-1)return dp[mon];
-        int ret=INT_MAX;
-        for(int i=0;i<coins.size();i++)
-        {  
-            if(mon-coins[i]>=0)
-            ret=min(ret*1LL,rec(dp,coins,mon-coins[i])+1LL);
+            return dp[0]=0;
+        if(dp[mon]!=-1)
+            return dp[mon];
+        int ans=INT_MAX;
+        for(auto it:coins)
+        {
+            if(mon-it>=0)
+                ans=min(ans*1LL,rec(dp,coins,mon-it)+1LL);
         }
-        dp[mon]=ret;
-        return dp[mon];
+        return dp[mon]=ans;
     }
     int coinChange(vector<int>& coins, int amount) {
         vector<int>dp(amount+1,-1);
-        int ans=rec(dp,coins,amount);
-        if(ans==INT_MAX)
-            return -1;
-        return ans;
+        return rec(dp,coins,amount)==INT_MAX?-1:dp[amount];
     }
 };

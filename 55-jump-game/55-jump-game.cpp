4726ddc7036513d6d52d3c63b1bf3e85636179c8
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-06-08 21:01:51 +0530

    Time: 91 ms (54.18%), Space: 48.4 MB (44.08%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,15 +1,38 @@
 class Solution {
 public:
+    // bool solve(int i,vector<int>&nums,vector<int>&dp)
+    // {
+    //     if(i>=nums.size()-1)return true;
+    //     if(dp[i]!=-1)
+    //         return dp[i];
+    //     for(int jump=1;jump<=nums[i];jump++)
+    //     {
+    //         if(solve(i+jump,nums,dp))
+    //             return dp[i]=1;
+    //     }
+    //     return dp[i]=0;
+    // }
     bool canJump(vector<int>& nums) {
-         int n=nums.size();
-        if(n==1)return true;
-         int indx=0;
-        for(int i=0;i<=indx;i++)
+        // bool isPre=false;
+        // for(auto it:nums)
+        //     if(it==0){
+        //         isPre=true;
+        //         break;
+        //     }
+        // if(!isPre)return true;
+        // vector<int>dp(nums.size(),-1);
+        // return solve(0,nums,dp);
+        
+        
+        
+        
+        
+        int idxMax=0;
+        for(int i=0;i<=idxMax;i++)
         {
-            if(i>=n)
-                return true;
-            indx=max(indx,i+nums[i]);
+            if(idxMax>=nums.size()-1)return true;
+            idxMax=max(idxMax,nums[i]+i);
         }
-        return indx>=n-1;
+        return idxMax>=nums.size()-1;
     }
 };

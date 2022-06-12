bfd113bea3a197f84faac2c7a49c8ee5d8a7a55f
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-06-12 16:44:40 +0530

    Time: 606 ms (100.00%), Space: 80.5 MB (100.00%) - LeetHub

--- 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    int minPathCost(vector<vector<int>>& grid, vector<vector<int>>& moveCost) {
-        int row=grid.size();
-        int col=grid[0].size();
-        vector<int>sum=grid[0];
-        //stores the sum of paths to reach this column i
-        
-        
-        //The basic idea is to store the value of best path for each column
-        for(int i=1;i<row;i++)
-        {   
-            vector<int>sums(col,INT_MAX);
-            for(int j=0;j<col;j++)
-            {
-                //from every column in above row we can jump to kth column
-                for(int k=0;k<col;k++)
-                {
-                    int tojump= sum[j];//jumping from j
-                        tojump+=grid[i][k];
-                    tojump+= moveCost[grid[i-1][j]][k];
-                    sums[k]=min(sums[k],tojump);
-                }
-            }
-            sum=sums;
+    int dfs(int i,int j,vector<vector<int>>&grid,vector<vector<int>>&Cost,vector<vector<int>>&dp)
+    {
+        if(i>=grid.size())
+            return 0;
+        if(i==grid.size()-1)
+            return grid[i][j];//cos aur aage nhi jana aur iss index pe aye hai to sum me add to hoga
+        int tmp=INT_MAX;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        for(int col=0;col<grid[0].size();col++)
+        {
+            int curr= grid[i][j]+ Cost[grid[i][j]][col] + dfs(i+1,col,grid,Cost,dp);
+            //col is the column to which you jump
+            tmp=min(tmp,curr);
         }
-        int mi=INT_MAX;
-        for(auto it:sum)
-            mi=min(mi,it);
-        return mi;
-        
+        return dp[i][j]=tmp;
+    }
+    int minPathCost(vector<vector<int>>& grid, vector<vector<int>>& Cost) {
+        //top down
+        int ans=INT_MAX;
+        int R=grid.size();
+        int C=grid[0].size();
+        vector<vector<int>>dp(R,vector<int>(C,-1));
+        for(int col=0;col<C;col++) //for each column we find what will be the path to end at that col
+            ans=min(ans,dfs(0,col,grid,Cost,dp));
+        return ans;
     }
 };

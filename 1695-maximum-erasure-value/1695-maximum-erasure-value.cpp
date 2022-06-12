9e61562310094557d635eabbc7dc1b0682a3710b
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-06-12 07:46:56 +0530

    Time: 628 ms (30.99%), Space: 137.1 MB (10.45%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
-    int solve(int l,int r,vector<int>&nums)
-    {
-        int su=0;
+    int solve(int l,int r,vector<int>&psum){
         if(l==0)
-            su=nums[r];
+            return psum[r];
         else
-        su= nums[r]-nums[l-1];
-        return su;
+            return psum[r]-psum[l-1];
     }
     int maximumUniqueSubarray(vector<int>& nums) {
-       int l=0;int r=0;
-        unordered_set<int>st;
-        int ans=0;
         vector<int>psum;
         psum.push_back(nums[0]);
         for(int i=1;i<nums.size();i++)
-        {
             psum.push_back(psum.back()+nums[i]);
-        }
+        
+        int ma=INT_MIN;
+        int l=0;
+        int r=0;
+        unordered_set<int>st;
+        //contains all unique elements
+        
         while(l<nums.size() and r<nums.size())
-        {   
-            
+        {
             if(st.find(nums[r])!=st.end())
             {
                 st.erase(nums[l]);
-                l++; 
+                l++;
             }
             else
-            {
+            {   
+                ma=max(ma,solve(l,r,psum));
                 st.insert(nums[r]);
-                int sum= solve(l,r,psum);
-                ans=max(sum,ans);
                 r++;
             }
         }
-        return ans;
+        return ma;
+        
     }
 };

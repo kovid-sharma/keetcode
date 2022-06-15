7b082dd098d33a9ad4da0c40980175de774d001a
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-06-15 15:20:46 +0530

    Time: 205 ms (27.77%), Space: 27.8 MB (19.65%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    static bool comp(string s1,string s2)
+    {
+        return s1.size()<s2.size();//true means correct configuration
+    }
+    int longestStrChain(vector<string>& words) {
+        //iterative dp is better
+        unordered_map<string,int>preExist;
+        for(auto it:words)
+            preExist[it]=1;
+        sort(words.begin(),words.end(),comp);
+        // for(auto it:words)
+        //     cout<<it<<endl;
+        // return 0;
+        
+        int maxx=1;
+        
+        for(auto str:words)
+        {
+            int sz=str.size();
+            
+            
+            /*predecessor of str can be made by deletions*/
+            
+            
+            for(int tmp=0;tmp<str.size();tmp++)
+            {   string now;
+                for(int i=0;i<str.size();i++)
+                {
+                    if(i==tmp)
+                        continue;
+                    now.push_back(str[i]);
+                    
+                }
+                
+                if(preExist.find(now)!=preExist.end())
+                {
+                    preExist[str]=max(preExist[str],preExist[now]+1);
+                   
+                }
+                 maxx=max(maxx,preExist[str]);
+            }
+            
+            
+        }
+        return maxx;
+    }
+};

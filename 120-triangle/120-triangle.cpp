8074edb47ed2be53d9849fa51b1ab429350f02b8
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-06-15 15:48:08 +0530

    Time: 26 ms (5.85%), Space: 12.3 MB (8.28%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    unordered_map<string,int>dp;
-    int solve(int r,int c,vector<vector<int>>&grid)
+    unordered_map<int,int>mp;
+    int solve(int i,int j,vector<vector<int>>&t)
     {
-        //basically solve definition is to calculate the minimum path from {i,j}
-        
-        
-        if(r==grid.size()-1)
-            return grid[r][c];
-        string str=to_string(r)+" "+to_string(c);
-        if(dp.find(str)!=dp.end())
-            return dp[str];
-        
-        return dp[str]=grid[r][c]+min(solve(r+1,c,grid),solve(r+1,c+1,grid));
+        if(i==t.size()-1)
+            return t[i][j];
+        if(j>=t[i].size())
+            return 0;
+        int encrypt=pow(i,2)+237*t[i][j]-pow(j,2)+pow(i,3);
+        if(mp.find(encrypt)!=mp.end())
+            return mp[encrypt];
+        return mp[encrypt]=t[i][j]+min(solve(i+1,j,t),solve(i+1,j+1,t));
     }
     int minimumTotal(vector<vector<int>>& triangle) {
+        //top-down
+        
         return solve(0,0,triangle);
     }
 };

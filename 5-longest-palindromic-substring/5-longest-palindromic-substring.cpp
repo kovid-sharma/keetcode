a8d7440ea7fa33d516b99803aefd85327af8d839
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-06-16 20:33:22 +0530

    Time: 1173 ms (7.67%), Space: 387.2 MB (5.33%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,46 +1,42 @@
 class Solution {
 public:
-    bool isPal(string tmp)
+    int ms=0;int me=0;//var for max r-l+1 string
+    int ans=1;
+    int solve(int s,int e,vector<vector<int>>&dp,string &str)
     {
-        int st=0;
-        int la=tmp.size()-1;
-        while(st<=la and tmp[st]==tmp[la])
-        {
-            la--;
-            st++;
-        }
-        if(st>=la)
+        if(s>=e)
             return true;
-        return false;
+        if(dp[s][e]!=-1)
+            return dp[s][e];
+        if(str[e]==str[s] and solve(s+1,e-1,dp,str))
+        {
+            
+            ans=max(ans,e-s+1);
+            if(ans==e-s+1)
+            {
+                ms=s;
+                me=e;
+            }
+            return dp[s][e]=1;
+        }
+        else
+        {   
+            solve(s+1,e,dp,str);
+            solve(s,e-1,dp,str);
+            return dp[s][e]=0;
+        }
     }
     string longestPalindrome(string s) {
-        int len=0;
         int n=s.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
         for(int i=0;i<n;i++)
-        {
-            int left=i;
-            int right=i;
-            while(left>=0 and right<n and s[left]==s[right])
-            {
-                len=max(right-left+1,len);
-                left--;
-                right++;
-            }
-            left=i;
-            right=i+1;
-            while(left>=0 and right<n and s[left]==s[right])
-            {
-                len=max(right-left+1,len);
-                left--;
-                right++;
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            string tmp=s.substr(i,len);
-            if(isPal(tmp))
-                return tmp;
-        }
-        return s;
+            dp[i][i]=1;
+        //dp stores -1 if not solve
+        //stores 1 if substring btw i and j is palindrome
+        //stores 0 is substring btw i and j is not.
+        
+        
+        solve(0,n-1,dp,s);
+        return s.substr(ms,me-ms+1);
     }
 };

1a642bfdea18b204dc537d86fed88d540cf128e0
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-06-29 23:17:17 +0530

    Time: 12 ms (57.94%), Space: 7.7 MB (33.87%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -1,33 +1,33 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-       //method 2//first was to use map to store that we have reached that listnode once or not this method is standard method using slow and fast pointers what we do is that at some point if there is a cycle the fast and slow eill become same if there is no cycle there will be no cycle and we will return null but after they are matched and there is a cycle we will be resetting our slow pointer to the head
-        if(head==NULL or head->next==NULL)return NULL;
+        if(head==NULL)return NULL;
+        if(head->next==NULL)return NULL;//no cycle in a single noded list
         
-        ListNode*slower=head;
-        ListNode*fasteer=head;
-        while(fasteer!=NULL and fasteer->next!=NULL)
+        
+        ListNode*first=head;
+        ListNode*secon=head;
+        bool isCycle=false;
+        while(secon!=NULL)
         {
-            slower=slower->next;
-            fasteer=fasteer->next->next;
-            if(fasteer==slower)
+            first=first->next;
+            if(secon->next==NULL)
+                return NULL;
+            secon=secon->next->next;
+            if(first==secon)
+            {
+                isCycle=true;
                 break;
+            }
         }
-        slower=head;
-        while(fasteer!=NULL and fasteer->next!=NULL)
-        {   
-            if(slower==fasteer)return slower;
-            slower=slower->next;
-            fasteer=fasteer->next;
+        if(!isCycle)
+            return NULL;
+        secon=head;
+        while(secon!=first)
+        {
+            secon=secon->next;
+            first=first->next;
         }
-        return NULL;
+        return secon;
     }
 };

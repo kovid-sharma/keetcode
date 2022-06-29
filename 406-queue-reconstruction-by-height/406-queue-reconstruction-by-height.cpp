a5be3376ca0f97d56c79f2dc58f09be37a4ac20b
Kovid Sharma <76726175+kovid-sharma@users.noreply.github.com>
2022-06-29 13:22:15 +0530

    Time: 68 ms (86.14%), Space: 11.5 MB (100.00%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
-    static bool comp(vector<int>a,vector<int>b)
+    static bool comp(vector<int>&a,vector<int>&b)
     {
         if(a[0]==b[0])
-            return a[1]<b[1];
-        return a[0]>b[0];
+        {
+            return a[1]>b[1];
+        }
+        return a[0]<b[0];
     }
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        /*This is still brute force*/
+        /*The logic is simple if any cell is empty then it means there is a greater height number           incoming at that position*/
         int n=people.size();
-        vector<vector<int>>ans;
         sort(people.begin(),people.end(),comp);
-        for(int i=0;i<n;i++){
-            // cout<<people[i][0]<<' '<<people[i][1]<<'}';
-            ans.insert(ans.begin()+people[i][1],people[i]);
-            
+        vector<vector<int>>ans(n);
+        // cout<<people[0][0];
+        // for(auto it:people)
+        //     cout<<it[0]<<" "<<it[1];
+        for(int i=0;i<people.size();i++)
+        {
+            int toChk=people[i][1];
+            int curr=0;
+            for(int j=0;j<people.size();j++)
+            {   
+                if(curr==toChk and ans[j].size()==0)
+                {   
+                    ans[j]=people[i];
+                    break;
+                }
+                if(ans[j].size()==0)
+                {
+                    curr++;
+                }
+                
+            }
         }
         return ans;
     }
